234d8b256986b51441fe18578265cb842d0892cf
Zuvo007 <53573536+Zuvo007@users.noreply.github.com>
2020-07-06 17:39:15 +0530

    spoj bytelandian gold coin

---
+++ sourcecodes/bytelandian-gold-coins.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<map>
+using namespace std;
+map <int,long long> dp;
+long long Exchange(long long n)
+{
+if(n==0)
+return n;
+if(dp[n]!=0)
+return dp[n];
+else
+{
+long long tmp=Exchange(n/2)+Exchange(n/3)+Exchange(n/4);
+if(tmp>n)
+dp[n]=tmp;
+else
+dp[n]=n;
+return dp[n];
+}
+}
+int main()
+{
+int t;
+long long n;
+while(scanf("%lld",&n)==1)
+{
+printf("%lld\n",Exchange(n));
+}
+return 0;
+}
