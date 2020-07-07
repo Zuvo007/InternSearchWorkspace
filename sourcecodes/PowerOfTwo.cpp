5014467b4aee539187927477f37a28e2b10dc706
Zuvo007 <53573536+Zuvo007@users.noreply.github.com>
2020-07-07 09:11:37 +0530

    update power of two

---
+++ sourcecodes/PowerOfTwo.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool PowerOfTwo(int n)
+{
+    if(n==0)
+        return false;
+    while(n!=1)
+    {
+        if(n%2!=0)
+            return false;
+        else
+            n=n/2;
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter a number :";
+    cin>>num;
+
+    if (PowerOfTwo(num))
+     cout<<"yes"<<endl;
+    else
+        cout<<"no";
+    return 0;
+}
