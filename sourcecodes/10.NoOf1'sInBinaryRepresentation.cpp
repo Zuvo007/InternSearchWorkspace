3acece183bc5f00e8f0128a4272c6f9bc77384b3
Zuvo007 <53573536+Zuvo007@users.noreply.github.com>
2020-07-07 10:32:50 +0530

    Day 3 Problem

---
+++ sourcecodes/10.NoOf1'sInBinaryRepresentation.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+ void BinaryConversion(int array[],int n)
+{
+    int i=0,j,count=0;
+    while(n>0)
+    {
+        array[i]=n%2;
+        n=n/2;
+        i++;
+    }
+    cout<<"binary conversion is :";
+    for(j=i-1;j>=0;j--)
+    {
+        cout<<array[j];
+        if(array[j]==1)
+            count++;
+    }
+    cout<<endl<<"The total no of 1's is :"<<count;
+
+}
+
+int main()
+{
+    int a[100],i,n;
+    cout<<"enter a  decimal number :";
+    cin>>n;
+   BinaryConversion(a,n);
+
+
+}
